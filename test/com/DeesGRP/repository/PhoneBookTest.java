77fe0755b4168dede180b7ed5c8070811906c39f
iabdullahi <iabdullahi@digicoreltd.com>
2022-02-09 23:42:06 +0100

    I implemented a feature to block contact by providing the contact id

--- test/com/DeesGRP/repository/PhoneBookTest.java
+++ test/com/DeesGRP/repository/PhoneBookTest.java
@@ -1,23 +1,49 @@
 package com.DeesGRP.repository;
 
+import com.DeesGRP.model.Contact;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class PhoneBookTest {
 
     @BeforeEach
     void setUp() {
+
     }
 
     @AfterEach
     void tearDown() {
     }
 
-    @Test
-    @DisplayName("")
-    void testSome(){
 
+
+    @Test
+    @DisplayName("Contact can be blocked")
+    void accountCanBeBlocked(){
+        //Given that
+        PhoneBook phoneBook = new PhoneBook();
+        Contact esther = new Contact("Esther", "90975674647");
+        Contact adeola = new Contact("Adeola", "646464764746");
+        List<Contact> contacts = new ArrayList<>(Arrays.asList(esther, adeola));
+        phoneBook.setContacts(contacts);
+
+        //When
+        phoneBook.blockContact(esther.getId());
+
+        //Assertion
+        assertEquals(contacts.size(), 1);
+        assertEquals(contacts.get(0).getFirstName(), adeola.getFirstName());
+        assertEquals(contacts.get(0).getLastName(), adeola.getLastName());
+        assertNotNull(contacts.get(0).getId());
+        assertEquals(phoneBook.getBlockContact().size(), 1);
+        assertTrue(phoneBook.getBlockContact().get(0).isBlock());
     }
 }
